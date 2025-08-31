144014ac581d70cbe44934962b4a623ad706da5b
SoftwareDev <163558479+JayChauhan2@users.noreply.github.com>
2025-08-31 16:11:49 +0000

    AI api integrated

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 import secrets
+from groq import Groq
 from flask import Flask, render_template, request, url_for, redirect
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Groq(
+    api_key=os.getenv('groq_api_key')
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ab8fbbb4d79d4120031e203d83d298e0' 
 
 secret_key = secrets.token_hex(16)
-print(secret_key)
+# print(secret_key)
 
 # Home page route
 @app.route('/')
@@ -53,3 +60,23 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
+
+def createLLMResponse(inp):
+    try:
+        completion = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+            {
+                "role": "user",
+                "content": "You are a trauma-informed virtual assistant designed to support veterans with PTSD. Always speak in a calm, respectful, and supportive tone. Prioritize emotional safety." + inp
+            }
+            ],
+            temperature=1,
+            max_completion_tokens=1024,
+            top_p=1,
+            stream=false,
+            stop=None
+        )
+        return completion.choices[0].message
+    except:
+        return "Unable to load summary"
