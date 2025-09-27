2cc1386ca3e37b5c3b87b08b7a118cd28a04e50a
JayChauhan2 <163558479+JayChauhan2@users.noreply.github.com>
2025-09-27 10:27:36 -0400

    ai chatbot functioning

--- static/script.js
+++ static/script.js
@@ -1,36 +1,51 @@
-// async function createLLMResponse(message) {
-//     try {
-//         const response = await fetch("/getLLMResponse", {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify({ message: message }),
-//         });
+async function createLLMResponse(message) {
+    try {
+        const response = await fetch("/getLLMResponse", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message: message }),
+        });
         
-//         console.log(response);
+        const data = await response.json();
 
-//         const data = await response.json();
+        if (data.response) {
+            return data.response;
+        } else {
+            return data.error;
+        }
+    } catch (err) {
+        return err;
+    }
+}
 
-//         if (data.response) {
-//             console.log("Response:", data.response);
-//         } else {
-//             console.error("Error:", data.error || "Unknown error");
-//         }
-//     } catch (err) {
-//         console.error("Fetch error:", err);
-//     }
-// }
+const usrMsgHolder = document.getElementById('usr-msg-holder')
+if (usrMsgHolder) {
+    const usrMsgBox = usrMsgHolder.querySelector('textarea')
+    const usrMsgSend = usrMsgHolder.querySelector('button')
+    const msgTemplate = document.getElementById('msg')
+    const msgZone = document.getElementById('msg-zone')
+    usrMsgSend.addEventListener("click", async function() {
+        let usrMsg = usrMsgBox.value.trim()
+        if (!(usrMsg.length > 0)) return;
+        
+        const userClone = msgTemplate.content.cloneNode(true)
+        const userCloneDiv = userClone.querySelector('div')
+        userCloneDiv.classList.add('user')
+        userCloneDiv.querySelector('div').innerText = usrMsg
+        usrMsgBox.value = "";
+        msgZone.appendChild(userClone)
 
-// createLLMResponse("hi");
-
-// const usrMsgBox = document.getElementById('msgtobot')
-// if (usrMsgBox) {
+        const botClone = msgTemplate.content.cloneNode(true)
+        botClone.querySelector('div').querySelector('div').innerText = await createLLMResponse(usrMsg)
+        msgZone.appendChild(botClone)
     
-// }
+    })
+}
 
 const calendarAdd = document.getElementsByClassName('calendar-add')[0]
 if (calendarAdd) {
     calendarAdd.onclick = function () {
-        const clone = document.getElementById('createEventBundle').content.cloneNode(true);
-        document.getElementsByTagName('main')[0].appendChild(clone);
+        const clone = document.getElementById('createEventBundle').content.cloneNode(true)
+        document.getElementsByTagName('main')[0].appendChild(clone)
     }
 }
