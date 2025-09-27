26d7e31d45183d60e61007e7b261b696965d017c
JayChauhan2 <163558479+JayChauhan2@users.noreply.github.com>
2025-09-26 20:29:22 -0400

    ai chat styling

--- static/script.js
+++ static/script.js
@@ -1,31 +1,31 @@
-async function createLLMResponse(message) {
-    try {
-        const response = await fetch("/getLLMResponse", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ message: message }),
-        });
+// async function createLLMResponse(message) {
+//     try {
+//         const response = await fetch("/getLLMResponse", {
+//             method: "POST",
+//             headers: { "Content-Type": "application/json" },
+//             body: JSON.stringify({ message: message }),
+//         });
         
-        console.log(response);
+//         console.log(response);
 
-        const data = await response.json();
+//         const data = await response.json();
 
-        if (data.response) {
-            console.log("Response:", data.response);
-        } else {
-            console.error("Error:", data.error || "Unknown error");
-        }
-    } catch (err) {
-        console.error("Fetch error:", err);
-    }
-}
+//         if (data.response) {
+//             console.log("Response:", data.response);
+//         } else {
+//             console.error("Error:", data.error || "Unknown error");
+//         }
+//     } catch (err) {
+//         console.error("Fetch error:", err);
+//     }
+// }
 
-createLLMResponse("hi");
+// createLLMResponse("hi");
 
-const usrMsgBox = document.getElementById('msgtobot')
-if (usrMsgBox) {
+// const usrMsgBox = document.getElementById('msgtobot')
+// if (usrMsgBox) {
     
-}
+// }
 
 const calendarAdd = document.getElementsByClassName('calendar-add')[0]
 if (calendarAdd) {
